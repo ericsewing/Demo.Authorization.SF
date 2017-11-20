ac9588f04dd9e69280d6d81a526f75f98e175d2a
Eric Ewing <eric-ewing@pluralsight.com>
2017-11-20 08:34:30 -0700

    Code cleanup

--- Authorization.WebApi/Program.cs
+++ Authorization.WebApi/Program.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.ServiceFabric.Services.Runtime;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace Authorization.WebApi
 {
     internal static class Program
     {
         /// <summary>
-        /// This is the entry point of the service host process.
+        ///     This is the entry point of the service host process.
         /// </summary>
         private static void Main()
         {
--- Authorization.WebApi/WebApi.cs
+++ Authorization.WebApi/WebApi.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Fabric;
 using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
@@ -14,21 +10,22 @@ using Microsoft.ServiceFabric.Services.Runtime;
 namespace Authorization.WebApi
 {
     /// <summary>
-    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    ///     The FabricRuntime creates an instance of this class for each service type instance.
     /// </summary>
     internal sealed class WebApi : StatelessService
     {
         public WebApi(StatelessServiceContext context)
             : base(context)
-        { }
+        {
+        }
 
         /// <summary>
-        /// Optional override to create listeners (like tcp, http) for this service instance.
+        ///     Optional override to create listeners (like tcp, http) for this service instance.
         /// </summary>
         /// <returns>The collection of listeners.</returns>
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
-            return new ServiceInstanceListener[]
+            return new[]
             {
                 new ServiceInstanceListener(serviceContext =>
                     new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
@@ -36,15 +33,15 @@ namespace Authorization.WebApi
                         ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
 
                         return new WebHostBuilder()
-                                    .UseKestrel()
-                                    .ConfigureServices(
-                                        services => services
-                                            .AddSingleton<StatelessServiceContext>(serviceContext))
-                                    .UseContentRoot(Directory.GetCurrentDirectory())
-                                    .UseStartup<Startup>()
-                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
-                                    .UseUrls(url)
-                                    .Build();
+                            .UseKestrel()
+                            .ConfigureServices(
+                                services => services
+                                    .AddSingleton(serviceContext))
+                            .UseContentRoot(Directory.GetCurrentDirectory())
+                            .UseStartup<Startup>()
+                            .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                            .UseUrls(url)
+                            .Build();
                     }))
             };
         }
